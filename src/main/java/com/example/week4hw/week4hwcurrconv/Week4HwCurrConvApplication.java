f0838d60731c5a8240c06a08046b914a3536908f
Hemant <hem789ant1997@outlook.com>
2026-02-05 12:36:57 +0530

    repo created

---
+++ src/main/java/com/example/week4hw/week4hwcurrconv/Week4HwCurrConvApplication.java
@@ -0,0 +1,13 @@
+package com.example.week4hw.week4hwcurrconv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week4HwCurrConvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week4HwCurrConvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/week4hw/week4hwcurrconv/Week4HwCurrConvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.week4hw.week4hwcurrconv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week4HwCurrConvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
